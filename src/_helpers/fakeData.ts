3d7b73d97121fea2320719b6b7bf324c10a4d113
KLCho <feelool007@gmail.com>
2019-08-03 22:46:31 +0800

    1. Add error page.

--- src/_helpers/fakeData.ts
+++ src/_helpers/fakeData.ts
@@ -1,4 +1,4 @@
-import { Dashboard, Home } from "@material-ui/icons";
+import { Dashboard, Home, SentimentVeryDissatisfied } from "@material-ui/icons";
 
 import { PageGroup, Page } from "../components/Sidebar";
 
@@ -11,6 +11,7 @@ export const pageGroups: Array<PageGroup> = [
 
 export const pages: Array<Page> = [
   { name: "index", to: "/", icon: Home },
+  { name: "Error Page", to: "/some-error-page", icon: SentimentVeryDissatisfied },
   { name: "Notifications", to: "/notifications", groupName: "Components" },
   { name: "Buttons", to: "/buttons", groupName: "Components" },
   { name: "Selection Controls", to: "/selection-controls", groupName: "Components" },
